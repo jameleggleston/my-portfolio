427f8eee8829001526bbf841aee602274f5288d2
God of War <jamel.eggleston@gmail.com>
2021-11-12 12:20:50 -0500

    updated RouteLink.js and Technology.js

--- src/components/RouteLink.js
+++ src/components/RouteLink.js
@@ -1,1 +1,11 @@
-//start here
+import { Link } from 'react-router-dom';
+
+const RouteLink = ({ route, text }) => {
+    return (
+        <div className='contact__link'>
+            <Link to={route}><button className='button button--flex'>{text}</button></Link>
+        </div>
+    )
+}
+
+export default RouteLink;
--- src/components/Technology.js
+++ src/components/Technology.js
@@ -1,1 +1,13 @@
-//start here
+import React from 'react'
+
+const Technology = ({ icon, techTitle, techSubtitle }) => {
+    return (
+        <div className='techs__data'>
+            {icon}
+            <h3 className='techs__title'>{techTitle}</h3>
+            <span className='techs__subtitle'>{techSubtitle}</span>
+        </div>
+    )
+}
+
+export default Technology
