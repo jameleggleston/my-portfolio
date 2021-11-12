f8ccc765b663e77adb641991b520979a1fd7f01f
God of War <jamel.eggleston@gmail.com>
2021-11-12 14:04:06 -0500

    updated project.js layout

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,1 +1,19 @@
-//start here
+import Project from '../Project';
+
+const Projects = () => {
+    return (
+        <section className='section'>
+            <h2 className='section__title section__title-gradient projects__line'>My <br /> Projects</h2>
+            <div className='projects__container container grid'>
+                <Project project={'Redux Store'} technologies={''} description={""} link={''} github={''} gif={''} />
+                <Project project={'Workout Tracker'} technologies={''} description={""} link={''} github={''} gif={''} />
+                <Project project={'Mix N Match'} technologies={""} description={''} link={''} github={''} gif={''} />
+                <Project project={'Note Taker'} technologies={''} description={""} link={''} github={''} gif={''} />
+                <Project project={'Sky Gazer'} technologies={""} description={""} link={''} github={''} gif={''} />
+                <Project project={'Weather Dashboard'} technologies={''} description={''} link={''} github={''} gif={''} />
+            </div>
+        </section>
+    )
+}
+
+export default Projects 
