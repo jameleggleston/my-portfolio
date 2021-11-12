cf9c3317405d0abe44ec5dc72a6937b5df9275e1
God of War <jamel.eggleston@gmail.com>
2021-11-12 11:42:36 -0500

    created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,14 @@
-//start here
+import RouteLink from './RouteLink';
+
+const Footer = ({ route, text }) => {
+    return (
+        <footer className='footer section'>
+            <RouteLink route={route} text={text} />
+            <div className='footer__copy'>
+                <a className='footer__copy-link' href="https://github.com/jameleggleston" target="_blank" rel='noreferrer'>&#169; Jamel Eggleston. All rights reserved</a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
